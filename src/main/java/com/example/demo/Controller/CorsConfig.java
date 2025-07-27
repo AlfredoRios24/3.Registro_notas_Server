a0d2a4a453ee5b2d98ce0dde65464e28b6501187
AlfredoRios24 <alfredoerios_03@hotmail.com>
2025-07-27 20:11:48 +0200

    permisos del fronted

--- src/main/java/com/example/demo/Controller/CorsConfig.java
+++ src/main/java/com/example/demo/Controller/CorsConfig.java
@@ -6,13 +6,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
-
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedOrigins("https://3-registro-notas-client.vercel.app")
+                .allowedOrigins(
+                        "https://3-registro-notas-client.vercel.app",
+                        "https://3-registro-notas-client-grr1duho9-alfredorios24s-projects.vercel.app"
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
-
     }
 }
