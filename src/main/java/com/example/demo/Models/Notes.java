2e3a7855d2612d18e8414d3d831354ac021c16c2
AlfredoRios24 <alfredoerios_03@hotmail.com>
2025-07-27 19:39:30 +0200

    refactor etiquetas en Notes

--- src/main/java/com/example/demo/Models/Notes.java
+++ src/main/java/com/example/demo/Models/Notes.java
@@ -1,8 +1,17 @@
 package com.example.demo.Models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 import java.time.LocalDateTime;
 
+@Entity
 public class Notes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 
     private Long id;
     private String title;
